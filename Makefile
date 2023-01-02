c1ebe8198497928cb24d2f92fa5247318e1a7e79
happyearthbytes <joe.dipilato@gmail.com>
2023-01-02 13:21:48 -0500

    add initial ansible scripts

--- Makefile
+++ Makefile
@@ -8,9 +8,18 @@ include scripts/make/extras.mak
 
 .PHONY: help
 
-run_a_command: ## Run a command
-	@echo todo
-run_another_command: ## Run another command
-	@echo todo
+test: # test Provision
+	@./provision/ansible/scripts/test.sh
+provision-build: # Provision build image
+	@./provision/ansible/scripts/build.sh
+provision-init: # Provision init
+	@./provision/ansible/scripts/init.sh
+provision-view-config: provision-generate # view ansible config
+	@./provision/ansible/scripts/view_config.sh
+provision-generate: # Provision generate
+	@./provision/ansible/scripts/generate.sh
+provision: ## Provision
+	@./provision/ansible/scripts/provision.sh
+
 run_an_advanced_command: # Run an advanced command
 	@echo todo
--- provision/ansible/scripts/init.sh
+++ provision/ansible/scripts/init.sh
@@ -4,5 +4,15 @@ __THIS_PATH="$( cd -- "$(dirname "${BASH_SOURCE}")" >/dev/null 2>&1 || exit ; pw
 __SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 __BASE_PATH="$( cd -- "${__SCRIPT_PATH}/../.." >/dev/null 2>&1 || exit ; pwd -P )"
 cd $__BASE_PATH/ansible
-podman run -it --rm -v${__BASE_PATH}:/localhost ansible ansible-config init --disabled > ansible.cfg.init
-cp ansible.cfg.init ansible.in.cfg
+
+CONFIG_FILE=ansible.cfg
+
+are_you_sure() {
+    read -p "Overwrite ${CONFIG_FILE}? [y/n] " RESPONSE
+    [ "${RESPONSE}" != "n" ] && [ "${RESPONSE}" != "y" ] && are_you_sure
+    [ "${RESPONSE}" != "y" ] && echo "Exiting" && exit 1
+}
+
+[ -f  ${CONFIG_FILE} ] && are_you_sure
+
+podman run --rm -v${__BASE_PATH}:/localhost --env "ANSIBLE_CONFIG=/etc/ansible/ansible.cfg" ansible ansible-config init --disabled > ansible.cfg.in
