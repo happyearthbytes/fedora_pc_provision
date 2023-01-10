1be69caaf40ae9246e9085861fc0a18f676677fa
happyearthbytes <joe.dipilato@gmail.com>
2023-01-09 20:48:44 -0500

    clean k3s gitignore

--- apps/k3s/scripts/k3s-clean-all.sh
+++ apps/k3s/scripts/k3s-clean-all.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 _ROOT="../../.." _COMMON="scripts/libs/bash/common/common.sh"
 . $( cd -- "$(dirname "${BASH_SOURCE[0]}")/${_ROOT}" >/dev/null 2>&1 || exit ; pwd -P )/${_COMMON}
-find ${__BASE_PATH}/apps/k3s \( -name 'base' -o -name 'charts' \) -maxdepth 2 -type d -exec echo rm -rf {} \;
+__print_header
+find ${__BASE_PATH}/apps/k3s -maxdepth 2 -type d \( -name 'base' -o -name 'charts' \) -exec rm -rf {} \;
--- apps/k3s/scripts/k3s-generate-all.sh
+++ apps/k3s/scripts/k3s-generate-all.sh
@@ -2,4 +2,4 @@
 _ROOT="../../.." _COMMON="scripts/libs/bash/common/common.sh"
 . $( cd -- "$(dirname "${BASH_SOURCE[0]}")/${_ROOT}" >/dev/null 2>&1 || exit ; pwd -P )/${_COMMON}
 __print_header
-# __run_section sudo systemctl start k3s
+find ${__BASE_PATH}/apps/k3s -maxdepth 2 -type f -name 'generate.sh' -exec {} \;
