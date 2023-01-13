287c2f81fb7f0f4303d09e6c0bea01dbe0d50bba
happyearthbytes <joe.dipilato@gmail.com>
2023-01-13 14:50:52 -0500

    add pip

--- apps/setup_scripts/install.sh
+++ apps/setup_scripts/install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-packages=packages.txt
-external=./external_installs.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${SCRIPT_DIR}/..
+
+packages=${SCRIPT_DIR}/packages.txt
+external=${SCRIPT_DIR}/external_installs.sh
+extra_setup=${SCRIPT_DIR}/extra_setup.sh
 
 print_install() {
     echo "# ==============================================================================="
@@ -13,5 +17,4 @@ sudo dnf install -y $(cat $packages | tr '\n' ' ')
 print_install "external packages"
 ${external}
 print_install "extra setup"
-extra_setup=./extra_setup.sh
 ${extra_setup}
